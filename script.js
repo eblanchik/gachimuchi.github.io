6b777a031dd829cb509b9bc25a8ea067d9159897
eblanchik <72874091+eblanchik@users.noreply.github.com>
2020-11-16 05:58:04 +0200

    Ухудшил сайт.

---
+++ script.js
@@ -0,0 +1,6 @@
+function gay(){
+var warning = confirm("Вы уверены, что хотите продолжить? Если вы слабонервный лучше нажмите на cancel");
+if(warning==true){
+    document.location.replace("http://www.site.ru");
+}
+}
